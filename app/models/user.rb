7f28eecd16e231381d5d5f3a0ca59452d5aeb723
EC2 Default User <ec2-user@ip-172-31-6-7.ec2.internal>
2020-11-18 04:31:40 +0000

    [Fix]背景色を、全てのページに適応

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  has_many :stress, dependent: :destroy
+  has_many :stresses, dependent: :destroy
 end

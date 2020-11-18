a0bef5ca8eba10c24be75d779b9af36abaa340a0
EC2 Default User <ec2-user@ip-172-31-6-7.ec2.internal>
2020-11-18 07:08:36 +0000

    [Add]新規投稿機能と、一覧ページの追加

--- app/controllers/stresses_controller.rb
+++ app/controllers/stresses_controller.rb
@@ -1,24 +1,34 @@
 class StressesController < ApplicationController
- 
+
   def index
+    @stresses = Stress.all
   end
-  
+
   def show
-  end 
-  
-  def new 
-  end 
-  
-  def create
   end
-  
-  def edit 
-  end 
-  
+
+  def new
+    @stress = Stress.new
+  end
+
+  def create
+    @stress = Stress.new(stress_params)
+    @stress.user_id = current_user.id
+    @stress.save!
+    redirect_to stresses_path
+  end
+
+  def edit
+  end
+
   def update
-  end 
-  
+  end
+
   def destroy
-  end 
-  
+  end
+
+  private
+  def stress_params
+    params.require(:stress).permit(:user, :title, :body)
+  end
 end

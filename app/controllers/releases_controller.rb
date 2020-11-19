c49c2cda6775346ae5234f80dbd9856c532d96c0
EC2 Default User <ec2-user@ip-172-31-6-7.ec2.internal>
2020-11-19 01:45:09 +0000

    [Add]マイページの追加

---
+++ app/controllers/releases_controller.rb
@@ -0,0 +1,27 @@
+class ReleasesController < ApplicationController
+  before_action :authenticate_user!
+  
+  def index
+  end
+
+  def eat
+  end
+
+  def buy
+  end
+
+  def travel
+  end
+
+  def listen
+  end
+
+  def watch
+  end
+
+  def laugh
+  end
+
+  def cry
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+  get 'releases/index'
   devise_for :users
   root "homes#top"
   resources :stresses
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
+  get "releases/eat", "releases#eat"
+  get "releases/buy", "releases#buy"
+  get "releases/travel", "releases#travel"
+  get "releases/listen", "releases#listen"
+  get "releases/watch", "releases#watch"
+  get "releases/laugh", "releases#laugh"
+  get "releases/cry", "releases#cry"
+  resources :releases, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

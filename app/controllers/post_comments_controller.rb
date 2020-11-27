a91055dca2e8aea56386bb552762da8fa1ecd16d
EC2 Default User <ec2-user@ip-172-31-6-7.ec2.internal>
2020-11-27 08:19:07 +0000

    [Add]ストレス発散各ページに、画像を挿入

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -6,14 +6,12 @@ class PostCommentsController < ApplicationController
     @comment = current_user.post_comments.new(post_comment_params)
     @comment.stress_id = @stress.id
     @comment.save
-    # redirect_to stress_path(stress)
   end
 
   def destroy
     @stress = Stress.find(params[:stress_id])
     post_comment = @stress.post_comments.find(params[:id])
     post_comment.destroy
-    # redirect_to stress_path(params[:stress_id])
   end
 
   private

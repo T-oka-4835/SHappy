01260e6287c82033cd912855b24a1d688d8796c0
EC2 Default User <okalovereal35@yahoo.co.jp>
2020-12-04 06:50:39 +0000

    [Add]新規登録時のバリデーションを設定/stress/indexの写真を変更

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -16,11 +16,11 @@ class RelationshipsController < ApplicationController
 
   def followings
     user = User.find(params[:user_id])
-    @users = user.followings
+    @users = user.followings.page(params[:page]).reverse_order
   end
 
   def followers
     user = User.find(params[:user_id])
-    @users = user.followers
+    @users = user.followers.page(params[:page]).reverse_order
   end
 end

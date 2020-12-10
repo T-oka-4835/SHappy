78b289f714411ac023b65e49eb005df1416fb4fb
EC2 Default User <okalovereal35@yahoo.co.jp>
2020-12-10 15:13:09 +0000

    [Add]Topページのスライドの幅を変更

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -43,6 +43,7 @@ $(function() {
     });
   });
 
+// アクションが起きると、ハンバーガーを閉じる
   $('#manu a[href]').on('click', function(event) {
         $('.hamburger').trigger('click');
     });
@@ -53,7 +54,7 @@ $('a[href^="#"]').click(function() {
   var speed = 1000;
   var href = $(this).attr("href");
   var target = $(href == "#" || href == "" ? 'html' : href);
-  var position = target.offset().top;
+  var position = target.offset().top + -70;
   $('body,html').animate({
     scrollTop: position
   }, speed, 'swing');

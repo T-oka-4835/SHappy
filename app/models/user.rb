5e54d89a6f9c95a487edba32756fb0dd00553922
EC2 Default User <okalovereal35@yahoo.co.jp>
2020-12-11 01:58:52 +0000

    [Add]通知がない時に、文字が出るよう条件分岐を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,8 @@ class User < ApplicationRecord
   # 通知機能
   has_many :active_notifications, class_name: 'Notification', foreign_key: 'visitor_id', dependent: :destroy
   has_many :passive_notifications, class_name: 'Notification', foreign_key: 'visited_id', dependent: :destroy
+
+  # 通知機能
   def create_notification_follow!(current_user)
     temp = Notification.where(["visitor_id = ? and visited_id = ? and action = ? ",current_user.id, id, 'follow'])
     if temp.blank?

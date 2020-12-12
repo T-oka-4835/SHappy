ada448f6d124ed5af8a7059b03a2f4014e5513d6
EC2 Default User <okalovereal35@yahoo.co.jp>
2020-12-12 02:26:03 +0000

    [Fix]productionの修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,15 +92,15 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-
-  config.action_mailer.default_url_options = { :hosts => 'https://shappy.work' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:"smtp.gmail.com",
-    domain: 'gmail.com',
-    port:587,
-    user_name: Rails.application.credentials.gmail[:user_name],
-    password: Rails.application.credentials.gmail[:password],
-    authentication: :login
-  }
+  address:              'smtp.gmail.com',
+  port:                  587,
+  domain:               'gmail.com',
+  user_name:            ENV['SEND_MAIL'],
+  password:             ENV['SEND_MAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto:  true
+}
+
 end

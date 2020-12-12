f00b59e442eade28bebfeea9e0ff888d3f8b2671
EC2 Default User <okalovereal35@yahoo.co.jp>
2020-12-12 01:44:00 +0000

    [Fix]productionファイルのurlを修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  config.action_mailer.default_url_options = {  :hosts => 'https://shappy.work/' }
+  config.action_mailer.default_url_options = { :hosts => 'https://shappy.work' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:"smtp.gmail.com",
